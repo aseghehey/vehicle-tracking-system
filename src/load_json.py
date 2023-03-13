11912d201863055dc515b7b6b50ed92da56ca5df
easeghehey <emanuelaseghey9@gmail.com>
2023-03-13 15:57:50 -0400

    default date added and initial work on load json

---
+++ src/load_json.py
@@ -0,0 +1,8 @@
+
+from vehicles import *
+
+with open('data/inventory.json', 'r') as json_file:
+    args = []
+    for line in json_file:
+        pass
+    
--- src/users.py
+++ src/users.py
@@ -1,11 +1,11 @@
-import datetime
+from datetime import date
 class Users():
     def __init__(self, username='', password='', first_name='',last_name='', date_joined=None) -> None:
         self.username = username
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
-        # if not date_joined:  
+        if not date_joined: date_joined = date.today()
         self.date_joined = date_joined
 
 class Admin(Users):
