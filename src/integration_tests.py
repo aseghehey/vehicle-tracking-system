4604a93ce359bd270290553ab44035c54cd01b50
Kate <katemakenzie@usf.edu>
2023-04-12 11:04:20 -0400

    nothing significant

--- src/integration_tests.py
+++ src/integration_tests.py
@@ -1,24 +1,19 @@
-import json
-from main import * 
-from interface import *
-from session import Auth
-from users import *
-from bcolors import PrintFormat, bcolors
-
-#testcase 1: test the the interface for a customer buying a car
-run()
 
 
-#testcase 2: test for an employee adding a car to the inventory
+#testcase 1: customer buying a car
 
 
 
-#testcase 3: test for a user trying to buy a car that is not in the inventory
+#testcase 2: adding a car to inventory
 
 
 
-#testcase 4: test for employee trying to buy a car that has already been sold
+#testcase 3: removing a car from inventory
 
 
 
-#testcase 5: 
+#testcase 4: managing employees
+
+
+
+#testcase 5: managing customers
