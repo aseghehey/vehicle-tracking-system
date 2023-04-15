9b114853708b3a8ea587250b10ade4d905a2f1a2
ATAshmore <spidermonkey752@gmail.com>
2023-04-15 16:50:19 -0400

    reformat

--- src/PigeonBox/session.py
+++ src/PigeonBox/session.py
@@ -25,7 +25,52 @@ The Authenticate function iterates over the list of users to find a matching use
 and returns the user object if found.
 
 Methods:
+Name: __init__(self) -> None
+    # One-line description: Initializes an object of the class with two private attributes containing employee and admin data from a JSON file.
+    # General description: This is an initialization method of a class that loads employee and admin data from a JSON file and stores it in two private attributes.
+    # Typical calling example: obj = ClassName()
+    # Accessibility: This method is public.
+    # Function prototype: It does not take any argument and returns None.
 
+Name: ReturnEmployees(self)
+    # One-line description: Return the list of employees.
+    # General description: The ReturnEmployees function returns the list of employees stored in the instance variable
+    #    __employees__.
+    # Typical calling example:
+    #   company = Company()
+    #   employees = company.ReturnEmployees()
+    # Accessibility: Public.
+    # Function prototype:def ReturnEmployees(self) -> list:
+
+Name: ReturnAdmins(self)
+    # One-line description: Returns a list of administrator users.
+    # General description: This function returns the list of administrator users that was previously loaded from
+    #    a JSON file using the LoadUsers() function. The list is stored as a private attribute __admins__ of the class.
+    # Typical calling examples:
+    #   userManager = UserManager()
+    #   admins = userManager.ReturnAdmins()
+    #   print(admins)  # prints the list of administrator users
+    # Accessibility: Public.
+    # Function prototype: def ReturnAdmins(self) -> List[Dict[str, Any]]:
+
+Name: __init__(self) -> None
+    # One-line description: Initializes an object of a class and creates a combined list of employees and admins as a list of users.
+    # General description: This function is a constructor method for a class that inherits from a parent class. It initializes 
+        an object of the child class and creates a list of users that contains the combined list of employees and admins.
+    # Typical calling examples:
+    #   obj = ChildClass()
+    # Accessibility: Private
+    # Function prototype: def __init__(self) -> None:
+
+Name: Authenticate(self, username, password)
+    # One-line description: Authenticate a user with their username and password.
+    # General description: This method searches through a list of users to find a matching username and
+    #   password combination. If a match is found, the user object is returned.
+    # Typical calling examples:
+    #   auth = Authenticator()
+    #   user = auth.Authenticate("john.doe", "password123")
+    # Accessibility: Public method.
+    # Function prototype': def Authenticate(self, username, password) -> Union[User, None]:
 
 ////////////////
 DATA STRUCTURES:
@@ -57,73 +102,33 @@ The calling code will raise these exceptions.
 from parsers import readJson, writeJson
 
 
-#   handles the session management and authentication of users.
 class Session:
+    """handles the session management and authentication of users."""
 
-    # One-line description: Initializes an object of the class with two private attributes containing employee and admin data from a JSON file.
-    # General description: This is an initialization method of a class that loads employee and admin data from a JSON file and stores it in two private attributes.
-    # Typical calling example: obj = ClassName()
-    # Accessibility: This method is public.
-    # Function prototype: It does not take any argument and returns None.
     def __init__(self) -> None:
+        """ Initializes an object of the class with two private attributes containing employee and admin data from a JSON file."""
         self.__employees__, self.__admins__ = readJson.LoadUsers()
 
-
-
-    # One-line description: Return the list of employees.
-    # General description: The ReturnEmployees function returns the list of employees stored in the instance variable
-    #    __employees__.
-    # Typical calling example:
-    #   company = Company()
-    #   employees = company.ReturnEmployees()
-    # Accessibility: Public.
-    # Function prototype:def ReturnEmployees(self) -> list:
     def ReturnEmployees(self):
+        """Return the list of employees."""
         return self.__employees__
-    
 
-
-    # One-line description: Returns a list of administrator users.
-    # General description: This function returns the list of administrator users that was previously loaded from
-    #    a JSON file using the LoadUsers() function. The list is stored as a private attribute __admins__ of the class.
-    # Typical calling examples:
-    #   userManager = UserManager()
-    #   admins = userManager.ReturnAdmins()
-    #   print(admins)  # prints the list of administrator users
-    # Accessibility: Public.
-    # Function prototype: def ReturnAdmins(self) -> List[Dict[str, Any]]:
     def ReturnAdmins(self):
+        """Returns a list of administrator users."""
         return self.__admins__
 
 
-
-
-
-#   Extends the Session class to add authentication functionality.
 class Auth(Session):
+    """Extends the Session class to add authentication functionality."""
 
-    # One-line description: Initializes an object of a class and creates a combined list of employees and admins as a list of users.
-    # General description: This function is a constructor method for a class that inherits from a parent class. It initializes an object of the child class and creates a list of users that contains the combined list of employees and admins.
-    # Typical calling examples:
-    #   obj = ChildClass()
-    # Accessibility: Private
-    # Function prototype: def __init__(self) -> None:
     def __init__(self) -> None:
+        """Initializes an object of a class and creates a combined list of employees and admins as a list of users."""
         super().__init__()
         # Combines the lists of employees and admins into a single list of users
         self.__users__ = self.__employees__ + self.__admins__
 
-
-
-    # One-line description: Authenticate a user with their username and password.
-    # General description: This method searches through a list of users to find a matching username and
-    #   password combination. If a match is found, the user object is returned.
-    # Typical calling examples:
-    #   auth = Authenticator()
-    #   user = auth.Authenticate("john.doe", "password123")
-    # Accessibility: Public method.
-    # Function prototype': def Authenticate(self, username, password) -> Union[User, None]:
     def Authenticate(self, username, password):
+        """Authenticate a user with their username and password."""
         # Iterates over the list of users to find a matching username and password
         for user in self.__users__:
             if user.getUsername() == username and user.getPassword() == password:
