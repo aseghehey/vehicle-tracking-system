71f268287f30c71b2ab1e1caa23d7df14555b7d4
ATAshmore <spidermonkey752@gmail.com>
2023-04-15 02:48:51 -0400

    final comments

--- src/PigeonBox/status.py
+++ src/PigeonBox/status.py
@@ -6,10 +6,11 @@ PROGRAM status:   defines an Enum class to represent the status of a car(availab
 
 ////////////////
 PROGRAMMER: Emanuel Aseghehey emanueldejes@usf.edu
+DOCUMENTOR: Alexander Ashmore atashmore@usf.edu
 
 ////////////////
-VERSION 1: written [day] [month] 2023 by [firstInitial]. [lastName]
-REVISION [revision# ex: 1.1]: [day] [month] 2023 by [firstInitial]. [lastName] to [purpose of revision]
+VERSION 1: written 13 March 2023 by E. Aseghehey
+REVISION: revision history can be found on the project GitHub
 
 ////////////////
 PURPOSE:
@@ -45,6 +46,8 @@ No explicit error handling.
 
 from enum import Enum
 
+
+
 #   Enum class that defines the status of a car, and two functions that convert 
 #   between the string representation of a status and its corresponding enum value.
 class Status(Enum):
@@ -59,12 +62,30 @@ STATUS_DICT = { "available": Status.AVAILABLE,
                 "backorder": Status.BACKORDER,
                 "delivered": Status.DELIVERED}
 
-#   Converts a string representation of a status to its corresponding Status enum value.
+
+# One-line description: Convert a string representation of car status to its corresponding enum value.
+# General description: This function takes a string representation of a car status, such as "available" or "ordered", and returns the corresponding enum value from the predefined STATUS_DICT dictionary.
+#   If the input status string is not found in the dictionary, the function returns None.
+# Typical calling examples:
+#   strToStatus("available") returns Status.AVAILABLE.
+#   strToStatus("backorder") returns Status.BACKORDER.
+#   strToStatus("sold") returns None.
+# Accessibility: The function is defined globally and can be accessed from anywhere in the program.
+# Function prototype: def strToStatus(status: str) -> Union[Status, None]
 def strToStatus(status: str):
     if status not in STATUS_DICT: return
     return STATUS_DICT[status]
 
-#   Converts a Status enum value to its corresponding string representation.
+
+
+# One-line description: Converts a Status enum value to its string representation.
+# General description: This function takes in a Status enum value and returns its corresponding string representation based on
+#   the values defined in the STATUS_DICT dictionary.
+# Typical calling examples:
+#   StatusToStr(Status.AVAILABLE) returns "available"
+#   StatusToStr(Status.ORDERED) returns "ordered"
+# Accessibility: This function can be accessed anywhere within the codebase where the st module is imported.
+# Function prototype: def StatusToStr(status: Status) -> str:
 def StatusToStr(status: Status):
     for key, value in STATUS_DICT.items():
         if value == status:
