6c3f30d2b31042ee3c5fa2160d172d8d847f7210
ATAshmore <spidermonkey752@gmail.com>
2023-04-15 02:45:33 -0400

    final comments

--- src/parsers/writeJson.py
+++ src/parsers/writeJson.py
@@ -1,8 +1,74 @@
+####################################################################################################################
+'''
+////////////////
+PROGRAM writeJson:
+The purpose of the code is to write data to a JSON file based on the object type in an array, 
+and it determines the file path based on the object type, and serializes the data using the object's 
+serialization function before writing it to the file. 
+
+writes on customers, inventory, orders, users json files
+
+////////////////
+PROGRAMMER: Kate Anderson katemakenzie@usf.edu
+DOCUMENTOR: Alexander Ashmore atashmore@usf.edu
+
+////////////////
+VERSION 1: written 6 April 2023 by K. Anderson
+REVISION: revision history can be found on the project GitHub
+
+////////////////
+PURPOSE:
+The general purpose of this code is to provide a function called writeJson that writes data to a JSON file based on the type of object passed in the data parameter. 
+It determines the appropriate file path and serializes the data before writing it to the file. 
+This function is used to save various data types such as inventory, orders, customers, and users to their respective JSON files in the program.
+
+Methods:
+- writeJson(data): Function to write data to a JSON file based on object type. 
+    This function writes data to a JSON file based on the object type in an array. 
+    It determines the file path based on the object type and serializes the data using the 
+    object's serialization function before writing it to the file.
+
+////////////////
+DATA STRUCTURES:
+DataStructures:
+Arrays: the input data parameter in the writeJson() function is an array.
+JSON Objects: the serialized variable in the writeJson() function is a JSON object created from the data array using the json.dumps() function.
+
+Attributes:
+INVENTORY_PATH: A string representing the file path for the inventory JSON file.
+ORDERS_PATH: A string representing the file path for the orders JSON file.
+CUSTOMER_PATH: A string representing the file path for the customer JSON file.
+USERS_PATH: A string representing the file path for the users JSON file.
+INDENTATION: An integer representing the number of spaces to use for indentation in the serialized JSON output.
+
+////////////////
+ALGORITHM:
+None
+
+////////////////
+ERROR HANDLING:
+No explicit error handling.
+
+////////////////
+'''
+####################################################################################################################
+
 from PigeonBox import vehicles, users, orders
 from parsers.readJson import INVENTORY_PATH, ORDERS_PATH, CUSTOMER_PATH, USERS_PATH
 import json
 INDENTATION = 2
 
+
+
+
+# One-line description: Function to write data to a JSON file based on object type.
+# General description: This function writes data to a JSON file based on the object type in an array.
+#    It determines the file path based on the object type and serializes the data using the object's serialization function before writing it to the file.
+# Typical calling examples:
+#    writeJson(inventory) to write the inventory list to the inventory JSON file.
+#   writeJson(orders) to write the orders list to the orders JSON file.
+# Accessibility: This function is accessible from anywhere within the program.
+# Function prototype: def writeJson(data):
 def writeJson(data):
     '''
     This function writes data to a json filePath, it will determine what kind of data is being loaded and write to 
